81d620e06f347fcee10c141629e413c457504dda
abled007 <audreybledsoe3@gmail.com>
2022-04-23 10:51:52 -0700

    Created db

--- main_app/models.py
+++ main_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 #Create your models here.
 
 class Song(models.Model):
-    
+
     title = models.CharField(max_length=20)
     artist = models.CharField(max_length=60)
     img = models.CharField(max_length=500)
--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,24 @@ from .models import Song
 class Home(TemplateView):
     template_name = 'home.html'
 
+class Song:
+    def __init__(self, title, artist, audio_link):
+        self.title = title
+        self.artist = artist
+        self.audio_link = audio_link
+
+songs = [
+    Song('Kitchen', 'Kid Cudi', 'https://www.youtube.com/watch?v=DqFwWq-JxWk&list=RDDqFwWq-JxWk&start_radio=1'),
+]
+
 class SongList(TemplateView):
        template_name = 'songlist.html'
 
-  
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    title = self.request.Get.get('title')
+    if title != None:
+        context['songs'] = Song.objects.filter(title__icontains=title)
+    else:
+        context['songs'] = Song.objects.all()
+    return context
