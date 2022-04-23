6252e1512d854c90117ab14bdc1b921a5c2d7765
abled007 <audreybledsoe3@gmail.com>
2022-04-23 09:20:18 -0700

    added a song model

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('songs/', views.SongList.as_view(), name='song-list')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
+from .models import Song
+
 
 # Create your views here.
 class Home(TemplateView):
     template_name = 'home.html'
+
+class SongList(TemplateView):
+       template_name = 'songlist.html'
+
+  
