8ef9c7ad9d557ca6032ac32dd1da564760f50025
abled007 <audreybledsoe3@gmail.com>
2022-04-23 18:25:47 -0700

    Migration success

--- main_app/models.py
+++ main_app/models.py
@@ -6,8 +6,18 @@ class Song(models.Model):
 
     title = models.CharField(max_length=20)
     artist = models.CharField(max_length=60)
-    img = models.CharField(max_length=500)
     audio_link = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+class Playlist(models.Model):
+
+    title = models.CharField(max_length=20)
+    songs = models.ManyToManyField(Song)
 
     def __str__(self):
         return self.title
