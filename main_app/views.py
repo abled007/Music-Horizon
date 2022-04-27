4c407979baada9089fb9b1179fb38e2c5b882052
abled007 <audreybledsoe3@gmail.com>
2022-04-26 18:42:10 -0700

    Home css

--- main_app/views.py
+++ main_app/views.py
@@ -16,6 +16,12 @@ from django.utils.decorators import method_decorator
 class Home(TemplateView):
     template_name = 'home.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['songs'] = Song.objects.all()
+        return context
+
+
 @method_decorator(login_required, name='dispatch')
 class Playlist_Create(CreateView):
     model = Playlist
